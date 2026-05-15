7f1bc6194c4856d5f7d643966b401bd6566e68a4
Yehosuah Mendoza <yemeli1996@gmai.com>
2026-05-15 13:13:38 -0600

    configs

---
+++ backend/core/config.py
@@ -0,0 +1,5 @@
+class Settings:
+    PROJECT_TITLE: str = "Blogs"
+    PROJECT_VERSION: str = "0.1.0"
+
+settings = Settings()
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from core.config import settings
 
-app = FastAPI(tittle="Blog", version="0.1.0")
+app = FastAPI(
+    title=settings.PROJECT_TITLE,
+    version=settings.PROJECT_VERSION
+)
 
 @app.get("/")
 def hello():
-    return {"msg": "Hello FastApi"}
+    return {"msg": "Hello FastAPI"}
